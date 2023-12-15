02ebf89f20f04ccd796a456b843a28a075c02555
412140 <144371963+412140@users.noreply.github.com>
2023-12-15 12:18:57 -0800

    Update main.cpp
    basics from student list

--- main.cpp
+++ main.cpp
@@ -3,9 +3,66 @@
 #include <cstring>
 #include <iostream>
 
+/*
+ghazi abbas
+fall semester C++ second part of linked lists/student list project
+galbraith
+
+sources:
+
+*/
+
+void addStu();
+void delStu();
+void printStu();
+
 int main()
 {
-  
+  char cmd[20];
+  int stId;
+
+  cout << "ennter command: quit, add, print, delete";
+  while()
+  {
+    if(strcmp(cmd, "quit")==0)
+    {
+      
+    } else if(strcmp(cmd, "add")==0) {
+    
+    
+    
+    } else if(strcmp(cmd, "print")==0) {
+    
+    
+    }  else if(strcmp(cmd, "delete")==0) {
+      cout << "enter student to delete";
+      cin >> stId;
+      delStu();
+    
+    
+    }
+      
+  }
   
   return 0;
 }
+
+void addStu()
+{
+
+  
+}
+
+void delStu()
+{
+
+  
+}
+
+void printStu()
+{
+
+
+  
+}
+
